d168888af6b138d4f85ae6366202b565225ace8f
mdaftabh12 <mdaftabmth22@gmail.com>
2024-06-08 18:31:31 +0530

    Add complete Category functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 // Utiles
 import connectDB from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
-// import categoryRoute from "./routes/categoryRoute.js";
+import categoryRoute from "./routes/categoryRoute.js";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoute);
-// app.use("/api/category", categoryRoute);
+app.use("/api/category", categoryRoute);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
