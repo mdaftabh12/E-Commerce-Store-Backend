70638a1aff82db9dd618147cb6ac708bd42c98cc
mdaftabh12 <mdaftabmth22@gmail.com>
2024-06-09 09:40:44 +0530

    Update database connection settings for improved stability

--- config/db.js
+++ config/db.js
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const mongo_uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/aftabStore";
+dotenv.config();
 
+const mongo_uri =
+  `${process.env.MONGO_URI}/${process.env.DB_NAME}` ||
+  "mongodb://127.0.0.1:27017/aftabStore";
+
+  console.log(mongo_uri);
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongo_uri).then(() => {
-      console.log("Successfully connected to MongoDB 👍");
-    });
+    await mongoose
+      .connect(mongo_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log(`Successfully connected to MongoDB 👍`);
+      });
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
     process.exit(1);
